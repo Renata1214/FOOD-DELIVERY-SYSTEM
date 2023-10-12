165492a97540ccb8feb77733d459f3400c0cabb9
Renata Espinosa <114301354+Renata1214@users.noreply.github.com>
2023-10-12 09:31:04 +0000

    Classes check

--- main.cpp
+++ main.cpp
@@ -8,7 +8,19 @@
 
 using namespace std;
 
+//Class declarations
+class Business;
+class DeliveryPeople;
+class Customer;
+class IdData;
+class Receipt;
+
+//Random Function configuration
 srand(time(NULL));
+int rand_int(int a, int b)
+{
+return rand()%(b-a+1) + a;
+}
 
 #Class Definitions
 
@@ -17,54 +29,110 @@ class Business {
 private:
     vector <string> MenuOptions;
     vector <string> PricesMenu;
-    bool availableDriver;
+    vector <DeliveryPeople> DeliveryWorkers;
+    bool availableDelivery;
     bool confirmationOrd;
     int numberDrivers;
 
-
-
+    friend class Receipt;
 
 public:
+Business (string **ArrMenuPrices, int rows, int columns, int deliveryNum){
+
+    availableDelivery=false;
+    confirmationOrd=false;
+    numberDrivers=deliveryNum;
+    for (int i=0, i<rows; i++){
+        MenuOptions[i]= ArrMenuPrices[i][0];
+        PricesMenu[i]=ArrMenuPrices[i][1];
+    }
+
+    for (int j=0; j<deliveryNum; j++){
+        DeliveryPeople newObject();
+        DeliveryWorkers[j]= newObject;
+    }
+
+}
+
+//Setter and Getter Functions
+
+//Function to determine availability of Drivers
+bool DriverAvailability (){
+
+
+
+}
+
+
+//Function to determine whether order is accepted or not
 
 
 };
 
-class Customer: public  {
+class Customer: public idData {
 
     private:
 
     
+    friend class Receipt;
 
     public:
 
 
 
+    //Setter and Getter functions
+
+
+
 };
 
-class DeliveryPeople  {
+
+//Delivery details
+class DeliveryPeople : public Business{
 
 private:
 float xcoordinate;
 float ycoordinate;
 int id;
-static int IDnumber;
 bool availability;
+static int IDnumber;
+
 
 public:
 
 DeliveryPeople (){
     xcoordinate= rand ();
     ycoordinate= rand ();
+    availability= rand_int(0,1);
     id=IDnumber;
     IDnumber ++;
 }
 
+//setter and getter functions
+void setPosition(float x, float y){
+    xcoordinate=x;
+    ycoordinate=y;}
 
+void setavailability(bool value){
+    availability=value;
+}
 
+float getxpos (){
+    return xpos;
+}
+
+float getypos (){
+    return ypos;
+}
+
+bool getavailability (){
+    return availability;
+}
 
 };
 int DeliveryPeople:: IDnumber=0;
 
+//ID information of the customer
 class IdData {
 
 private:
@@ -74,9 +142,7 @@ string nameCustomer;
 int personalID;
 static int UniqueNumber;
 
-
 public:
-
 //Constructor
 idData(){
     xCusPos= 0;
@@ -87,7 +153,6 @@ idData(){
 }
 
 idData (float coordinatex, float coordinatey, string name){
-
     xCusPos= coordinatex;
     yCusPos = coordinatey;
     nameCustomer= name;
@@ -98,9 +163,29 @@ idData (float coordinatex, float coordinatey, string name){
 //Getter and Setter Functions
 
 
+};
+//Definition of Static Variable
+int IDdata:: UniqueNumber=0;
+
+
+//Receipt of the order
+class Receipt {
+    private:
+    float totalCost;
+    float deliveryFee;
+    
+
+    public:
+    Receipt (){
+
+        totalCost=0;
+        deliveryFee=0;
+    }
+
+    //Setter and Getter Functions
+
+
 
 
 
 };
-//Definition of Static Variable
-int IDdata:: UniqueNumber=0;
